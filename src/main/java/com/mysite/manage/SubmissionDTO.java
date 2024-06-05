8133ea38bef5337a1802df8cd9af9ab184191cba
nitge <return3210@gmail.com>
2024-06-05 12:56:32 +0900

    test

---
+++ src/main/java/com/mysite/manage/SubmissionDTO.java
@@ -0,0 +1,10 @@
+package com.mysite.manage;
+
+import lombok.Getter;
+
+@Getter
+public class SubmissionDTO {
+    private String username;
+    private String password;
+    private String code;
+}
