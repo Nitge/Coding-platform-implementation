b8c6ef1048e47d931428dab43ea9a23ef069b1e5
sunmok <tjsahr9191@naver.com>
2024-06-19 23:02:57 +0900

    [ :wrench: | fix ] FileService code 디렉토리 부재시 생성 코드 추가

--- manage/src/main/java/com/mysite/manage/file/FileService.java
+++ manage/src/main/java/com/mysite/manage/file/FileService.java
@@ -2,6 +2,7 @@ package com.mysite.manage.file;
 
 import org.springframework.stereotype.Service;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -9,7 +10,13 @@ import java.io.IOException;
 public class FileService {
 
     public void saveFile(Integer id, String code){
-        try(FileWriter fw = new FileWriter("code/"+id.toString()+".py")){
+        String directoryPath = "code";
+        File directory = new File(directoryPath);
+
+        if (!directory.exists()) {
+            directory.mkdirs();  // 디렉토리가 존재하지 않으면 생성
+        }
+        try(FileWriter fw = new FileWriter(directoryPath + "/" + id.toString() + ".py")){
             fw.write(code);
         }
         catch(IOException e){
