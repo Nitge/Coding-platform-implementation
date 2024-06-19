fd2ac69995c767f9d2612e7679fef50df0b46910
Nitge <42697796+Nitge@users.noreply.github.com>
2024-06-19 21:08:37 +0900

    Delete manage directory

--- manage/README.md
+++
@@ -1,1 +0,0 @@
-# Coding-platform-implementation
--- manage/src/main/java/com/mysite/manage/submission/UpdateDTO.java
+++
@@ -1,9 +0,0 @@
-package com.mysite.manage.submission;
-
-import lombok.Getter;
-
-@Getter
-public class UpdateDTO {
-    private int id;
-    private String status;
-}
